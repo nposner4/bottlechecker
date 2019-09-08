bdd369b5942b3a00062db3c17b87f46873ac4e33
Noah Posner <n.b.posner@student.tudelft.nl>
2019-09-09 00:50:12 +0200

    Create project template

---
+++ sem-minor-template/safescan/patient.py
@@ -0,0 +1,12 @@
+class Patient(object):
+
+    def __init__(self, name, surname, age, weight, gender, doctor, nurse, medicine):
+        """Return a new Patient."""
+        self.name = name
+        self.surname = surname
+        self.age = age
+        self.weight = weight
+        self.gender = gender
+        self.doctor = doctor
+        self.nurse = nurse
+        self.medicine = medicine
